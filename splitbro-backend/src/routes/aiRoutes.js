22678de01f876d4e5ebbf6ccb025602e636deb6c
cagataycndss <cagataycandas40@gmail.com>
2026-04-04 17:53:27 +0300

    fix

--- splitbro-backend/src/routes/aiRoutes.js
+++ splitbro-backend/src/routes/aiRoutes.js
@@ -5,9 +5,7 @@ import { verifyPriceSchema } from '../validations/aiValidation.js';
 
 const router = express.Router();
 
-// Geliştiren: Furkan Kasalak
 router.post('/verify-price', validate(verifyPriceSchema), verifyPrice);
-// Geliştiren: Çağatay Candaş
 router.post('/item-categorization', categorizeItems);
 
 export default router;
