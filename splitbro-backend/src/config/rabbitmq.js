d5642e792ecdc9e1ddbfad73db8c84e01db825b5
gkdz417 <gk.erten125@gmail.com>
2026-06-09 19:01:50 +0300

    Update

--- splitbro-backend/src/config/rabbitmq.js
+++ splitbro-backend/src/config/rabbitmq.js
@@ -11,6 +11,8 @@ export const connectRabbitMQ = async () => {
     console.log('RabbitMQ Connected');
 
     await channel.assertQueue('ai_categorization_queue', { durable: true });
+    
+    await channel.assertQueue('ai_receipt_scan_queue', { durable: true });
   } catch (error) {
     console.error('RabbitMQ Connection Error:', error);
   }
